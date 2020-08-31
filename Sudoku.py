a4207126dcf226d42476d43df4cd9547b087686c
VaibhavModi <vaibhavmodi9842@gmail.com>
2020-08-31 15:46:43 +0530

    Added_GUI

--- Sudoku.py
+++ Sudoku.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+
 def row_check(array,x,number_toenter):
 	for row_element in array[x]:
 		if row_element==number_toenter:
@@ -51,29 +53,57 @@ def solution(sudoku_array):
 			sudoku_array[x][y]=0
 			 
 	return False
+
+class draw_window():
+	global entrybox_list
+	entrybox_list=[]
+
+	def __init__(self, master):
+		master.geometry('500x520')
+		master.title('Sudoku Solver')
+		master.resizable(False, False)
+		self.upper_frame(master)
+		self.lower_frame(master)
+
+	def upper_frame(self,master):
+		global button1
+		frame1=tk.Frame(master)
+		label1 = tk.Label(frame1,text='Testing this thing here',font=('Arial',12)).grid(row=0)
+		button1 = tk.Button(frame1,text='Click here to enter a number', command=lambda: self.insert()).grid(row=1)
+		frame1.grid(column=0,row=0, padx=10,pady=10,sticky='w')
+
+	#Sudoku 9x9 Board
+	def lower_frame(self,master):
+		frame2 = tk.Frame(master)
+		for row in range(9):
+			entrybox_list.append([])
+			for column in range(9):
+				temp = tk.Entry(frame2, width=3,justify='center',font=('Arial',17))
+				temp.grid(column=column,row=row,ipady=5,ipadx=5)
+				entrybox_list[row].append(temp)
+		frame2.grid(column=0,row=1,padx=10,pady=(70,20))
+
+	def insert(self):
+		entrybox_list[0][1].insert(0,'ok')
+
 if __name__=='__main__':
-	sudoku_array=[[3, 0, 6, 5, 0, 8, 4, 0, 0], 
-			[5, 2, 0, 0, 0, 0, 0, 0, 0], 
-			[0, 8, 7, 0, 0, 0, 0, 3, 1], 
-			[0, 0, 3, 0, 1, 0, 0, 8, 0], 
-			[9, 0, 0, 8, 6, 3, 0, 0, 5], 
-			[0, 5, 0, 0, 9, 0, 6, 0, 0], 
-			[1, 3, 0, 0, 0, 0, 2, 5, 0], 
-			[0, 0, 0, 0, 0, 0, 0, 7, 4], 
-			[0, 0, 5, 2, 0, 6, 3, 0, 0]]
 
-	Answer_Key='''	3 1 6 5 7 8 4 9 2
-					5 2 9 1 3 4 7 6 8
-					4 8 7 6 2 9 5 3 1
-					2 6 3 4 1 5 9 8 7
-					9 7 4 8 6 3 1 2 5
-					8 5 1 7 9 2 6 4 3
-					1 3 8 9 4 7 2 5 6
-					6 9 2 3 5 1 8 7 4
-					7 4 5 2 8 6 3 1 9	'''
+	window_app= tk.Tk()
+	drawing_object=draw_window(window_app)
+	window_app.mainloop()
 
-	Result=solution(sudoku_array)
+	sudoku_array=[[0, 0, 6, 0, 0, 7, 0, 0, 0],
+			[2, 8, 5, 0, 0, 0, 0, 0, 0],
+			[0, 0, 4, 9, 0, 6, 0, 0, 0],
+			[1, 0, 0, 0, 0, 0, 0, 6, 0],
+			[0, 0, 0, 0, 1, 3, 9, 0, 0],
+			[0, 2, 0, 0, 0, 0, 4, 0, 0],
+			[4, 0, 8, 5, 0, 0, 0, 0, 2],
+			[0, 0, 0, 8, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 6, 0, 0, 8, 5]]
+
+	'''Result=solution(sudoku_array)
 	if Result:
 		print(sudoku_array)
 	else:
-		print('Error',Result)
+		print('Error',Result)'''
